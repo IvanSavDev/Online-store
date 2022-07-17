104daccea64e324a99ebc7b64d30aa11225228cc
IvanSavDev <ivan-savenko@mail.ru>
2022-07-17 11:26:02 +0300

    fix: small fix

--- online-store/src/context/ShopContext.tsx
+++ online-store/src/context/ShopContext.tsx
@@ -1,8 +1,50 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useReducer } from 'react';
+import { InitialState } from '../type/initialState';
+import { reducerShop } from '../reducer/reducer';
+import { Actions } from 'reducer/reducerActions';
 
-export const ShopContext = createContext<null>(null);
+export const ShopContext = createContext<null | {
+  valueShop: InitialState;
+  dispatch: React.Dispatch<Actions>;
+}>(null);
 
-export const Context = ({ children }) => {
-  const value = null;
-  return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
+export const Context = ({ children }: { children: React.ReactNode }) => {
+  const state: InitialState = {
+    selectedCompany: {
+      msi: false,
+      hp: false,
+      huawey: false,
+      honor: false,
+      lenovo: false,
+      acer: false,
+      asus: false,
+      apple: false,
+    },
+    selectedCpu: {
+      intel: false,
+      amd: false,
+      m1: false,
+    },
+    selectedRam: {
+      8: false,
+      16: false,
+    },
+    selectedSsd: {
+      256: false,
+      512: false,
+    },
+    selectedCounts: [],
+    selectedPrice: [],
+    selectedYearRealease: [],
+    selectedColors: {},
+    selectedFavorite: false,
+  };
+
+  const [valueShop, dispatch] = useReducer(reducerShop, state);
+
+  return (
+    <ShopContext.Provider value={{ valueShop, dispatch }}>
+      {children}
+    </ShopContext.Provider>
+  );
 };
--- online-store/src/index.tsx
+++ online-store/src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './component/App/App';
+import { Context } from './context/ShopContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 
-root.render(<App />);
+root.render(
+  <Context>
+    <App />
+  </Context>
+);
