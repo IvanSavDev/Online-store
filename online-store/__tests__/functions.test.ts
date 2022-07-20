f29367ed743a3d66e4961d0f879d34aee2179e91
IvanSavDev <ivan-savenko@mail.ru>
2022-07-20 13:46:40 +0300

    feat: add tests

--- online-store/__tests__/functions.test.ts
+++ online-store/__tests__/functions.test.ts
@@ -1,12 +1,24 @@
-import { chooseCompany } from '../src/reducer/reducerActions';
+import {
+  chooseCompany,
+  choosePrice,
+  chooseRam,
+  addToBacket,
+} from '../src/reducer/reducerActions';
 import { getFilteredCards } from 'Src/component/Cards/getFilteredCards';
 import { getSortedCards } from 'Src/component/Cards/getSortedCards';
 import { productData } from 'Src/data/productData';
 import { getUniqNames } from 'Src/component/Filters/MergeCategories/getUniqNames';
 import { reducerShop } from '../src/reducer/reducer';
+import {
+  SELECT_PRICE,
+  ADD_TO_BASKET,
+  SELECT_RAM,
+  SELECT_COMPANY,
+} from 'Src/reducer/reducerConst';
+import { InitialStateFiltersType } from 'Src/types/initialStateFiltersType';
 
 test('filter cards', () => {
-  const initState = {
+  const initState: InitialStateFiltersType = {
     selectedCompany: ['huawey'],
     selectedCpu: [],
     selectedRam: [],
@@ -26,6 +38,13 @@ test('filter cards', () => {
   expect(result).toBe(false);
 });
 
+test('choose ram', () => {
+  expect(chooseRam({ 8: true })).toStrictEqual({
+    type: SELECT_RAM,
+    payload: { 8: true },
+  });
+});
+
 test('sort cards', () => {
   const firstElement = productData[0];
   const secondElement = productData[1];
@@ -33,6 +52,13 @@ test('sort cards', () => {
   const result = getSortedCards('name-decrease');
 
   expect(result(firstElement, secondElement)).toBe(-1);
+});
+
+test('choose price', () => {
+  expect(addToBacket(5)).toStrictEqual({
+    type: ADD_TO_BASKET,
+    payload: 5,
+  });
 });
 
 test('get uniq name', () => {
@@ -45,11 +71,11 @@ test('choose company', () => {
   const insertArg = { msi: true };
   const result = chooseCompany(insertArg);
 
-  expect(result).toEqual({ type: 'SELECT_COMPANY', payload: insertArg });
+  expect(result).toEqual({ type: SELECT_COMPANY, payload: insertArg });
 });
 
 test('reset filters', () => {
-  const initState = {
+  const initState: InitialStateFiltersType = {
     selectedCompany: [],
     selectedCpu: [],
     selectedRam: [],
@@ -62,7 +88,7 @@ test('reset filters', () => {
     search: '',
   };
 
-  const state = {
+  const state: InitialStateFiltersType = {
     selectedCompany: ['huawey'],
     selectedCpu: [],
     selectedRam: ['8'],
@@ -76,4 +102,11 @@ test('reset filters', () => {
   };
 
   expect(reducerShop(state, { type: 'RESET' })).toStrictEqual(initState);
+});
+
+test('choose price', () => {
+  expect(choosePrice([20000, 40000])).toStrictEqual({
+    type: SELECT_PRICE,
+    payload: [20000, 40000],
+  });
 });
